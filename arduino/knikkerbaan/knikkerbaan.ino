e22c4722969e21a740e78c1447cdeb7e0e6d7c7c
Jamie13388 <57214999+Jamie13388@users.noreply.github.com>
2021-12-06 13:11:48 +0100

    Update knikkerbaan.ino
    extra constanten toegevoegd

--- arduino/knikkerbaan/knikkerbaan.ino
+++ arduino/knikkerbaan/knikkerbaan.ino
@@ -6,8 +6,13 @@ const int BOVEN_POORT_PIN = 7;    // pin van servo die bovenste poort regelt
 const int LEEGLOOP_TIJD = 5000;   // tijd die knikkers nodig hebben om na het
                                   // sluiten van de poort langs alle sensors te komen
 
+const int SERVO_RAD_PIN = #;        // pin van servo/motor? die rad bestuurd
+const int SERVO_FLIPPER_LINKS = #;  // pin van servo die linker flipper regelt
+const int SERVO_FLIPPER_RECHTS = #; // pin van servo die rechter flipper regelt
+
 char WIFI_NETWERK[] = "";
 char WIFI_WACHTWOORD[] = "";
 char SERVER_DOMEINNAAM[] = "";    // domeinnaam van gitpod-server, zoals 3000-lavendel-bla-bla.gitpod.io
 
 const int TELLER_A_PIN = 5;       // pin waarop IR-sensor voor Teller A is aangesloten
+ 
