2819b7ea927530fc8a9e26c589c4fb89eb244ff5
lisannee <57898234+lisannee@users.noreply.github.com>
2022-02-21 19:05:59 +0100

    molen server (niet de 360) code

--- arduino/valkuil_360servo/valkuil_360servo.ino
+++ arduino/valkuil_360servo/valkuil_360servo.ino
@@ -5,29 +5,28 @@
 Servo servo360;  // create servo object to control a servo
 // twelve servo objects can be created on most boards
 Servo servoOpvang;
+Servo servoMolen;
 Servo servoPoort1;
 Servo servoPoort2;
 
 int molenSnelheid = 85;    // variable to store the servo position
-int pos2;    // variable to store the servo position
-int pos3;    // variable to store the servo position
-int pos4;    // variable to store the servo position
 boolean poortIsOpen = false;
-boolean ledIsAan = false;
 unsigned long valluikTimer = 1000;
-unsigned long ledTimer = 100;
+unsigned long molenTimer = 1000;
+
 
 
 void setup() {
-  pinMode(13, OUTPUT);
   servo360.attach(12);  // attaches the servo on pin 9 to the servo object
-  servoOpvang.attach(11);  // attaches the servo on pin 9 to the servo object
+  servoOpvang.attach(8);  // attaches the servo on pin 9 to the servo object
+  servoMolen.attach(11);  // attaches the servo on pin 9 to the servo object
   servoPoort1.attach(10);  // attaches the servo on pin 9 to the servo object
   servoPoort2.attach(9);  // attaches the servo on pin 9 to the servo object
 
 // poorten zijn dicht
   servoPoort1.write(0);
   servoPoort2.write(180);
+  servoMolen.write(90);
 
   // 360 servo starten
   servo360.write(molenSnelheid);
@@ -49,13 +48,18 @@ void loop() {
       valluikTimer = millis() + 250;
     }
   }
-
   
-  
-
-  
-
-
-
-
-}
+  // kunnen niet twee loops samen dus oplossing hiervoor nog bedenken
+void loop(){
+  if (millis() > molenTimer) {
+    if (poortIsOpen) {
+      servoMolen.write(110);
+      poortIsOpen = false;
+      molenTimer = millis() + 250;
+    }
+    else {
+      servoMolen.write(180);
+      poortIsOpen = true;
+      molenTimer = millis() + 500;
+    }
+  }
