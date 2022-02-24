dbff0e068a50de6045fff9f9609ab23730561208
Jamie13388 <Jamie13388@gmail.com>
2022-02-24 15:59:47 +0000

    3 tellers, get 404

--- database/db-creation.sql
+++ database/db-creation.sql
@@ -5,11 +5,25 @@ CREATE TABLE runs (
   stamp DATETIME NOT NULL
 );
 
-CREATE TABLE sensorData (
+CREATE TABLE sensorData1 (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   run INTEGER NOT NULL,
   stamp DATETIME NOT NULL,
-  aantalKnikkers INTEGER
+  aantalKnikkers1 INTEGER
+);
+
+CREATE TABLE sensorData2 (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  run INTEGER NOT NULL,
+  stamp DATETIME NOT NULL,
+  aantalKnikkers2 INTEGER
+);
+
+CREATE TABLE sensorData3 (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  run INTEGER NOT NULL,
+  stamp DATETIME NOT NULL,
+  aantalKnikkers3 INTEGER
 );
 
 CREATE TABLE instellingen (
@@ -23,5 +37,7 @@ CREATE TABLE instellingen (
 /* Indien je standaard wat gegevens in de database wilt,
    voeg hieronder dan INSERT regels to */
 INSERT INTO runs (stamp) VALUES (CURRENT_TIMESTAMP);
-INSERT INTO sensorData (run, stamp, aantalKnikkers) VALUES (1, CURRENT_TIMESTAMP, 3);
+INSERT INTO sensorData1 (run, stamp, aantalKnikkers1) VALUES (1, CURRENT_TIMESTAMP, 6);
+INSERT INTO sensorData2 (run, stamp, aantalKnikkers2) VALUES (1, CURRENT_TIMESTAMP, 4);
+INSERT INTO sensorData3 (run, stamp, aantalKnikkers3) VALUES (1, CURRENT_TIMESTAMP, 5);
 INSERT INTO instellingen (run, stamp, wachttijdPoort) VALUES (1, CURRENT_TIMESTAMP, 15);
